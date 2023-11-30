c6e6f87d6cc0c462fed5c7505fb63a9c4b045b3b
Mateusz Szymusiak <123317043+bzimienny@users.noreply.github.com>
2023-11-30 19:55:12 +0100

    installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 dialog --title "Thanks for download" --msgbox"Witaj w instalacji TestOS\n GLHF\n developed by: Mateusz Szymusiak\n press ok to start" 10 70;
 cfdisk /dev/sda
-echo "Witaj w instalatorze TestOS wersja 1.1";
-echo "Enter-rozpocznij instalacje";
-read n;
 mkfs.ext4 /dev/sda3;
 mount /dev/sda3 /mnt;
 mkswap /dev/sda2;
