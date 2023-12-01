1f0a91aa562bc6505a98a79b04ae867a653fd5fd
Mateusz Szymusiak <123317043+bzimienny@users.noreply.github.com>
2023-12-01 20:58:30 +0100

    installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+clear
+status = 0
 dialog --title "Thanks for download" --msgbox"Witaj w instalacji TestOS\n GLHF\n developed by: Mateusz Szymusiak\n press ok to start" 10 70;
-cfdisk /dev/sda
-mkfs.ext4 /dev/sda3;
-mount /dev/sda3 /mnt;
-mkswap /dev/sda2;
-swapon /dev/sda2;
-pacstrap /mnt base linux linux-firmware;
-genfstab -U /mnt >> /mnt/etc/fstab;
-arch-chroot /mnt;
+cfdisk /dev/sda;
+commands=([0]="mkfs.ext4 /dev/sda3"\
+       	[1]="mount /dev/sda3 /mnt"\
+       	[2]="mkswap /dev/sda2"\
+       	[3]="swapon /dev/sda2"\
+       	[4]="pacstrap /mnt base linux linux-firmware"\
+       	[5]="genfstab -U /mnt"\
+       	[6]="archroot /mnt")
+for ((x=0;x<10;x++));
+do
+dialog --title "installation" --gauge "Please wait" 7 70 2>$status; 
+echo ${commands[$x]};
+((status+=16));
+done
 
 
 
